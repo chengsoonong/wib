6c82c5c69c8cd6ddf03157a45270a8b2260dbbdd
Cheng Soon Ong <chengsoon.ong@anu.edu.au>
2016-11-01 23:09:51 +1100

    refactor to pass debug as command line arg

--- dvdt/cli.py
+++ dvdt/cli.py
@@ -2,90 +2,103 @@ import subprocess
 import click
 
 class Repo(object):
-    def __init__(self, vc_name='git'):
+    def __init__(self, debug, vc_name='git'):
+        self.debug = debug
         self.vc_name = vc_name
 
-def _shell(command, debug=True):
-    if debug:
-        click.echo(command)
-        to_cont = input('Continue? [y/n]')
-        if to_cont != 'y':
-            return
-    subprocess.call(command, shell=True)
+    def shell(self, command):
+        if self.debug:
+            click.echo(command)
+            to_cont = input('Continue? [y/n]')
+            if to_cont != 'y':
+                return
+        subprocess.call(command, shell=True)
+
 
 @click.group()
 @click.pass_context
-def main(context):
-    context.obj = Repo()
+@click.option('--debug', is_flag=True, default=True)
+def main(context, debug):
+    context.obj = Repo(debug)
 
 @main.command()
-def init():
+@click.pass_context
+def init(context):
     """Start a new repository"""
     pass
 
 @main.command()
-def track(file_names):
+@click.pass_context
+def track(context, file_names):
     """Keep track of each file in list file_names"""
     pass
 
 @main.command()
-def untrack(file_names):
+@click.pass_context
+def untrack(context, file_names):
     """Forget about tracking each file in the list file_names"""
     pass
 
 @main.command()
 @click.argument('message')
 @click.option('--name', default='')
-def checkin(message, name):
+@click.pass_context
+def checkin(context, message, name):
     """Commit saved changes to the repository.
     message - commit message
     name    - tag name
     """
-    _shell('git commit -a -m "' + message + '"')
+    context.obj.shell('git commit -a -m "' + message + '"')
     if name != '':
-        _shell('git tag -a ' + name + ' -m "' + message + '"')
+        context.obj.shell('git tag -a ' + name + ' -m "' + message + '"')
 
 @main.command()
 @click.argument('file_names', nargs=-1)
-def checkout(file_names):
+@click.pass_context
+def checkout(context, file_names):
     """Revert each file in the list file_names back to version in repo"""
     if len(file_names) == 0:
         click.echo('No file names to checkout specified.')
         click.echo('The following have changed since the last check in.')
-        _shell('git status')
+        context.obj.shell('git status')
     for fn in file_names:
-        _shell('git checkout -- ' + fn)
+        context.obj.shell('git checkout -- ' + fn)
 
 @main.command()
 @click.option('--tags', is_flag=True, default=False)
-def upload(tags):
+@click.pass_context
+def upload(context, tags):
     """Synchronise local repo to remote repo"""
     if tags:
-        _shell('git push --tags')
+        context.obj.shell('git push --tags')
     else:
-        _shell('git push')
+        context.obj.shell('git push')
 
 @main.command()
-def download():
+@click.pass_context
+def download(context):
     """Synchronise remote repo to local repo"""
-    _shell('git pull')
+    context.obj.shell('git pull')
 
 @main.command()
-def status():
+@click.pass_context
+def status(context):
     """See which files have changed, checked in, and uploaded"""
-    _shell('git status')
+    context.obj.shell('git status')
 
 @main.command()
-def log():
+@click.pass_context
+def log(context):
     """See history"""
     format = "'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
-    _shell('git log --graph --pretty=format:' + format + ' --abbrev-commit --stat')
+    context.obj.shell('git log --graph --pretty=format:' + format + ' --abbrev-commit --stat')
 
 @main.command()
 @click.argument('file_name', default='')
-def diff(file_name):
+@click.pass_context
+def diff(context, file_name):
     """See changes that occured since last check in"""
-    _shell('git diff --color-words --ignore-space-change ' + file_name)
+    context.obj.shell('git diff --color-words --ignore-space-change ' + file_name)
 
 main.add_command(init)
 main.add_command(track)
